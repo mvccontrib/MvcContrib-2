0fbcb42b163df32d9663814cd20103bd7d835ba1
unknown <User@.(none)>
2009-09-23 10:32:48 -0500

    updated the release process

---
+++ LatestVersion/Release.ps1
@@ -0,0 +1,20 @@
+
+
+function DownloadFiles {
+    remove-item "MVCContrib.Extras.release.zip"
+    remove-item "MVCContrib.release.zip"
+    remove-item "MVCContrib.source.zip"
+    $extrasUrl = "http://teamcity.codebetter.com/guestAuth/repository/download/bt81/.lastPinned/MVCContrib.Extras.release.zip"
+    $releaseUrl = "http://teamcity.codebetter.com/guestAuth/repository/download/bt81/.lastPinned/MVCContrib.release.zip"
+    $sourceUrl = "http://teamcity.codebetter.com/guestAuth/repository/download/bt81/.lastPinned/MVCContrib.source.zip"
+
+    $clnt = new-object System.Net.WebClient
+
+    $clnt.DownloadFile($extrasUrl,"MVCContrib.Extras.release.zip")
+    $clnt.DownloadFile($releaseUrl,"MVCContrib.release.zip")
+    $clnt.DownloadFile($sourceUrl,"MVCContrib.source.zip")
+}
+
+DownloadFiles 
+& "..\bin\codeplex\createrelease.exe" "1.0.0.987 for MVC1"
+
