80ec15bc8d14ed0d4f6650aed06e3682175de957
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2009-10-31 20:03:11 +0800

    CastleBinder should not create an instance of the object if the modelname does not exist on the compositenode.

--- src/MvcContrib.Castle/CastleBinder.cs
+++ src/MvcContrib.Castle/CastleBinder.cs
@@ -66,7 +66,14 @@ namespace MvcContrib.Castle
 		{
             IDataBinder binder = LocateBinder(controllerContext);
             string modelName = Prefix ?? bindingContext.ModelName;
-            object instance = binder.BindObject(bindingContext.ModelType, modelName, Exclude, null, new TreeBuilder().BuildSourceNode(GetStore(controllerContext)));
+			var tree = new TreeBuilder().BuildSourceNode(GetStore(controllerContext));
+
+			if(tree.GetChildNode(modelName) == null)
+			{
+				return null;	
+			}
+
+			object instance = binder.BindObject(bindingContext.ModelType, modelName, Exclude, null, tree);
             return instance;
 		}
 
