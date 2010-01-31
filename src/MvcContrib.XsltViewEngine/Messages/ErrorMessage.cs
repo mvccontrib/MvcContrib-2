420ef3cc6cfc7f72b9b901bd76eef02f53be0b9f
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2010-01-31 17:52:43 +0000

    Removing XsltViewEngine - this is now in a separate repository http://github.com/mvccontrib/XsltViewEngine

--- src/MvcContrib.XsltViewEngine/Messages/ErrorMessage.cs
+++
@@ -1,8 +0,0 @@
-namespace MvcContrib.XsltViewEngine.Messages
-{
-    public class ErrorMessage : Message
-    {
-        public ErrorMessage(string message) : base(MessageType.Error, message){}
-        public ErrorMessage(string message, string controlId) : base(MessageType.Error, message, controlId){}
-    }
-}
--- src/MvcContrib.XsltViewEngine/Messages/IMessage.cs
+++
@@ -1,9 +0,0 @@
-namespace MvcContrib.XsltViewEngine.Messages
-{
-	public interface IMessage
-	{
-		string ControlID { get; set; }
-		string Content { get; set; }
-		MessageType MessageType { get; }
-	}
-}
