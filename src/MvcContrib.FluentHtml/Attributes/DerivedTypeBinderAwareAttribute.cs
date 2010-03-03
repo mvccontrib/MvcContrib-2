efe08f27983cd4af56cf953dfbd15d0ed21511a5
Steve Hebert <steve.hebert@gmail.com>
2010-03-03 10:06:11 -0600

    DerivedTypeModelBinder

---
+++ src/MvcContrib.FluentHtml/Attributes/DerivedTypeBinderAwareAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcContrib.FluentHtml.Attributes
+{
+    /// <summary>
+    /// This attribute is used to identify alternate types that should be considered by the default model
+    /// binder when running.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = true, Inherited = true)]
+    public class DerivedTypeBinderAwareAttribute : Attribute
+    {
+        /// <summary>
+        /// ctor describing a class that may be alternatively bound to a common base property
+        /// </summary>
+        /// <param name="derivedType"></param>
+        public DerivedTypeBinderAwareAttribute(Type derivedType)
+        {
+            DerivedType = derivedType;
+        }
+
+        /// <summary>
+        /// the type declared for binding
+        /// </summary>
+        public Type DerivedType
+        { get; private set; }
+
+    }
+}
---
+++ src/MvcContrib.FluentHtml/PartialNameConventionService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MvcContrib.FluentHtml
+{
+    /// <summary>
+    /// provides the naming service and ability to change naming convention for typed partials
+    /// </summary>
+    public static class PartialNameConventionService
+    {
+        private static string _partialNameConvention = "{0}TypePartial";
+
+        /// <summary>
+        /// Partial name convention for strongly typed views.
+        /// </summary>
+        public static string PartialNameConvention
+        {
+            get { return _partialNameConvention; }
+            set { _partialNameConvention = value; }
+        }
+
+        /// <summary>
+        /// Creates a partial name based on the name convention
+        /// </summary>
+        /// <param name="targetType">the targeted type to be rendered</param>
+        /// <returns>name according to convention</returns>
+        public static string GeneratePartialName(Type targetType)
+        {
+            return String.Format(_partialNameConvention, targetType.Name);
+        }
+    }
+}
