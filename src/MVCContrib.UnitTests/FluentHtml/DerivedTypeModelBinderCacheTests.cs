9bcce1cb94eb646f78d2feb0c8adb64c940e581d
Steve Hebert <steve.hebert@gmail.com>
2010-03-03 23:46:06 -0600

    Added tests for derivedtypebindecache

---
+++ src/MVCContrib.UnitTests/FluentHtml/DerivedTypeModelBinderCacheTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using MvcContrib.FluentHtml.Attributes;
+using MvcContrib.FluentHtml.Binders;
+using NUnit.Framework;
+
+namespace MvcContrib.UnitTests.FluentHtml
+{
+    [TestFixture]
+    [DerivedTypeBinderAware(typeof(int))]
+    public class DerivedTypeModelBinderCacheTests
+    {
+        [Test]
+        public void validate_declarative_registration_of_derived_types()
+        {
+            DerivedTypeModelBinderCache.RegisterDerivedTypes(typeof(DerivedTypeModelBinderCacheTests),
+                                                             new[] {typeof(string)});
+
+            Assert.That((from p in DerivedTypeModelBinderCache.GetDerivedTypes(typeof(DerivedTypeModelBinderCacheTests))
+                         where p.Name == typeof(string).Name
+                         select p).FirstOrDefault(), Is.Not.Null);
+
+            DerivedTypeModelBinderCache.Reset();
+
+            // next, let's validate that the cache was cleared by reset
+            Assert.That((from p in DerivedTypeModelBinderCache.GetDerivedTypes(typeof(DerivedTypeModelBinderCacheTests))
+                         where p.Name == typeof(string).Name
+                         select p).FirstOrDefault(), Is.Null);
+
+        }
+
+        [Test]
+        public void validate_attribute_scan_on_getDerivedTypes_call()
+        {
+            DerivedTypeModelBinderCache.Reset();
+
+            Assert.That((from p in DerivedTypeModelBinderCache.GetDerivedTypes(typeof(DerivedTypeModelBinderCacheTests))
+                             where p.Name == typeof(int).Name
+                             select p).FirstOrDefault(), Is.Not.Null);
+
+            DerivedTypeModelBinderCache.Reset();
+        }
+    }
+}
